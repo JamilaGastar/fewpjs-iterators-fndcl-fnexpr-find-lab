ec440b7301c4c7f3f2f8476796c09fd4694a7568
jamilagastar <jamilagastar@gmail.com>
2022-04-29 16:57:48 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
+
+
+superbowlWin = (array) => {
+    return array.find((recordForYear) => recordForYear.result === "W");
+}    
+
+console.log(superbowlWin(record));
 
