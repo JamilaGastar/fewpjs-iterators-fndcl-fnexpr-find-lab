8aabf75757fd339b03800ff19321d5c91ae2ac33
jamilagastar <jamilagastar@gmail.com>
2022-05-03 10:40:09 +1000

    partially completed lab

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const record = [
-    {year: "2018", result: "L"},
-    {year: "2017", result: "W"},
-    {year: "2016", result: "N/A"}
+    {year: "1950", result: "L"},
+    {year: "1969", result: "W"},
+    {year: "1980", result: "L"}
   ]
 
+let winningYears = record.find(recordForYear => recordForYear.result === "W")
+console.log(winningYears);
 
-superbowlWin = (array) => {
-    return array.find((recordForYear) => recordForYear.result === "W");
-}    
+superbowlWin = (array) => array.year;
+console.log(superbowlWin(winningYears));
+      
 
-console.log(superbowlWin(record));
+
+
+
+
 
